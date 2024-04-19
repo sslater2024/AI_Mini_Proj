f47e4e183e43a69b13a7eea0a6d974e0d813571f
sslater2024 <70551490+sslater2024@users.noreply.github.com>
2024-04-19 15:03:21 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,28 +1,7 @@
-import os
 from ultralytics import YOLO
+import os
+from ultralytics.data.converter import convert_coco
 
-
-def train():
-    print("training")
-    model = YOLO('yolov8n-class.pt')
-    #the yaml file is in the data folder
-    results = model(data= "./data/commands.yaml",show = True, conf= 0.4, save = True)
-    
-def validate():
-    model = YOLO('custom_model.pt')
-    metrics = model.val()
-    print("validate")
-
-def test():
-    model = YOLO('custom_model-class.pt')
-    # this is to see your work in action
-    # source=0 is your webcam if you have one, otherwise it won't work
-    results = model(source = 0,show = True, conf= 0.4, stream= True)
-
-
-if __name__=="__main__": 
-   # call the function that you want to use when running
-   # will probably change this to cli at some point for fancy points
-   # train()
-   # validate()
-   test()
+model = YOLO('yolov8n.yaml') # build a new model from scratch
+results = model.train(data = "config.yaml", epochs = 20, imgsz =640)
+# the model it trains is the runs/detect/ train/weights : best.pt
