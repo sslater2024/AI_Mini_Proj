b2c3f2862a5d18415593145aeb2e77bea6c188ec
sslater2024 <70551490+sslater2024@users.noreply.github.com>
2024-05-01 18:33:39 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
+# train.py
+# The purpose of this file is to train the model on however many epochs is deemed neccesary
 from ultralytics import YOLO
-import os
-from ultralytics.data.converter import convert_coco
 
-model = YOLO('yolov8n.yaml') # build a new model from scratch
-results = model.train(data = "config.yaml", epochs = 30, imgsz = 640)
+#model = YOLO('yolov8n.yaml') # build a new model from scratch
 # the model it trains is the runs/detect/ train/weights : best.pt
+model = YOLO('best.pt') # build a new model from scratch
+
+results = model.train(data = "config.yaml", epochs = 30, imgsz = 640)
+
